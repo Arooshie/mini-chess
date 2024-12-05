ef5ad98c87aa37d91e1ddb6c974a040e13ce1aa1
Arooshie <164707484+Arooshie@users.noreply.github.com>
2024-12-05 15:00:04 +0600

    Create Turn.cpp

---
+++ Turn.cpp
@@ -0,0 +1,26 @@
+char board[8][8];
+bool isWhiteTurn = true;
+
+boolk isFriendlyPiece(int row,int col){
+    char piece= board[row][col];
+    if (isWhiteTurn) {
+        return piece == 'WP' || piece == 'WQ' || piece == 'WK' || piece == 'WR'|| piece == 'WN'|| piece == 'WB' ;
+
+    }
+    else {
+        return piece == 'BP' || piece == 'BQ' || piece == 'BK' || piece == 'BR'|| piece == 'BN'|| piece == 'BB' ;
+
+    }
+}
+
+bool isOpponentPiece(int row,int col){
+    if (isWhiteTurn) {
+        return piece == 'BP' || piece == 'BQ' || piece == 'BK' || piece == 'BR'|| piece == 'BN'|| piece == 'BB' ;
+
+    }
+    else {
+        return piece == 'WP' || piece == 'WQ' || piece == 'WK' || piece == 'WR'|| piece == 'WN'|| piece == 'WB'  ;
+
+    }
+
+}
